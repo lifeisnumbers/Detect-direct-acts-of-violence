f077724c48414d268a936b6b077219c483bb7f5c
TuyetMinh14 <Lucybieber2003@gmail.com>
2024-01-15 15:24:44 +0700

    update predict and model.py

--- AI-MODEL/model.py
+++ AI-MODEL/model.py
@@ -1,32 +1,21 @@
-from skimage.transform import resize
-import numpy as np
+from __future__ import absolute_import
+from __future__  import division
+from __future__ import print_function
 import tensorflow as tf
-
-def video_mamonreader(frame):
-    frames = np.zeros((30, 160, 160, 3), dtype=np.float)
-    i=0
-    frm = resize(frame,(160,160,3))
-    frm = np.expand_dims(frm,axis=0)
-    if(np.max(frm)>1):
-        frm = frm/255.0
-    frames[i][:] = frm
-    i +=1
-    print("reading video")
-    while i < 30:
-        frm = resize(frame,(160,160,3))
-        frm = np.expand_dims(frm,axis=0)
-        if(np.max(frm)>1):
-            frm = frm/255.0
-        frames[i][:] = frm
-        i +=1
-    return frames
-
-def mamon_videoFightModel2(tf,wight='AI-MODEL/mamonbest947oscombo-drive.hdfs'):
-    layers = tf.contrib.keras.layers
-    models = tf.contrib.keras.models
-    losses = tf.contrib.keras.losses
-    optimizers = tf.contrib.keras.optimizers
-    metrics = tf.contrib.keras.metrics
+import numpy as np
+from skimage.io import imread
+from skimage.transform import resize
+import cv2
+import os
+from PIL import Image
+from io import BytesIO
+import time
+def mamon_videoFightModel2(tf,wight='AI-MODEL/mamonbest947oscombo.hdfs'):
+    layers = tf.keras.layers
+    models = tf.keras.models
+    losses = tf.keras.losses
+    optimizers = tf.keras.optimizers
+    metrics = tf.keras.metrics
     num_classes = 2
     cnn = models.Sequential()
     #cnn.add(base_model)
@@ -65,10 +54,52 @@ def mamon_videoFightModel2(tf,wight='AI-MODEL/mamonbest947oscombo-drive.hdfs'):
 
     return model
 
+np.random.seed(1234)
+
+model22 = mamon_videoFightModel2(tf)
+def video_mamonreader(cv2,filename):
+    frames = np.zeros((30, 160, 160, 3), dtype=np.float64)
+    i=0
+    print(frames.shape)
+    vc = cv2.VideoCapture(filename)
+    if vc.isOpened():
+        rval , frame = vc.read()
+    else:
+        rval = False
+    frm = resize(frame,(160,160,3))
+    frm = np.expand_dims(frm,axis=0)
+    if(np.max(frm)>1):
+        frm = frm/255.0
+    frames[i][:] = frm
+    i +=1
+    print("reading video")
+    while i < 30:
+        rval, frame = vc.read()
+        frm = resize(frame,(160,160,3))
+        frm = np.expand_dims(frm,axis=0)
+        if(np.max(frm)>1):
+            frm = frm/255.0
+        frames[i][:] = frm
+        i +=1
+    return frames
 def pred_fight(model,video,acuracy=0.9):
     pred_test = model.predict(video)
     if pred_test[0][1] >=acuracy:
         return True , pred_test[0][1]
     else:
         return False , pred_test[0][1]
+def main_fight(vidoss):
+    vid = video_mamonreader(cv2,vidoss)
+    datav = np.zeros((1, 30, 160, 160, 3), dtype=np.float64)
+    datav[0][:][:] = vid
+    millis = int(round(time.time() * 1000))
+    print(millis)
+    f , precent = pred_fight(model22,datav,acuracy=0.65)
+    millis2 = int(round(time.time() * 1000))
+    print(millis2)
+    res_mamon = {'fight':f , 'precentegeoffight':str(precent)}
+    res_mamon['processing_time'] =  str(millis2-millis)
+    return res_mamon
 
+# res = main_fight('videos/fight.mp4')
+# print(res)
--- AI-MODEL/predict-savevideo.py
+++ AI-MODEL/predict-savevideo.py
@@ -1,14 +1,14 @@
 import cv2
 from PIL import Image
-import config
 import numpy as np
 from kafka import KafkaConsumer, KafkaProducer
 from model import *
 
 
-in_topic_name = "camera_in"
-# out_topic_name = "video_out"
-kafka_ip='' #insert ip 
+in_topic_name = "Camera_in"
+out_topic_name = "prediction_value"
+out_video_topic_name = "save_video"
+kafka_ip='localhost:9092' #insert ip 
 consumer = KafkaConsumer(
     in_topic_name,
     bootstrap_servers=kafka_ip,
@@ -21,34 +21,50 @@ consumer = KafkaConsumer(
 
 # Kafka Producer
 producer = KafkaProducer(
-    bootstrap_servers=[config.ip],
+    bootstrap_servers=kafka_ip,
     max_request_size=9048576,
     compression_type = 'gzip'
 )
-model = mamon_videoFightModel2(tf, wight='AI-MODEL/mamonbest947oscombo-drive.hdfs')
 
 
-frames = np.zeros((30, 160, 160, 3), dtype=np.float)
-i = 0 
+model = mamon_videoFightModel2(tf)
+
+print('running')
+
+
 for message in consumer:
     stream = message.value
+    #chuyển thành hình ảnh
+
     image = cv2.imdecode(np.frombuffer(stream, dtype=np.uint8), cv2.IMREAD_COLOR)
     print(image.shape)
 
     cv2.putText(image, "PROCESSED", (100,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255),2)
-    print("Send to kafka")
+    # print("Send to kafka")
     ret, frame = cv2.imencode('.jpg', image)
-    while i < 30:
+    print(frame)
+    if ret:   
         frm = resize(frame,(160,160,3))
         frm = np.expand_dims(frm,axis=0)
         if(np.max(frm)>1):
             frm = frm/255.0
-        frames[i][:] = frm
-        i +=1
+    print(frm)
+   
+frames = np.zeros((30, 160, 160, 3), dtype=np.float)
+i = 0 
+# 30 frames đầu tiên
+
+while True:
+    if i < 30:
+        kq, percent = pred_fight(model, frames, acuracy=0.6)
+    elif i == 29:
+        frames = np.delete(frames,0,axis=0)
+        frames[i][:] = frm         
     kq, percent = pred_fight(model, frames, acuracy=0.6)
     print(kq)
     print(frames)
-
-  
-    # producer.send( out_topic_name , buffer.tobytes())#.encode("utf-8"))
-    producer.flush()
+    # if kq: 
+    #         # producer.send(out_video_topic_name, frames.tobytes())
+    #         producer.send( out_topic_name , kq)#.encode("utf-8"))
+    # producer.send(out_video_topic_name, frame.tobytes)
+    # producer.flush()
