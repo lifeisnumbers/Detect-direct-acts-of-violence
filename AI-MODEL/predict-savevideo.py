219f0e3a62a3448c444858138951f53464c724e3
TuyetMinh14 <Lucybieber2003@gmail.com>
2024-01-15 15:42:12 +0700

    update predict

--- AI-MODEL/predict-savevideo.py
+++ AI-MODEL/predict-savevideo.py
@@ -31,7 +31,9 @@ model = mamon_videoFightModel2(tf)
 
 print('running')
 
-
+frames = np.zeros((30, 160, 160, 3), dtype=float)
+nodatav = np.zeros((1, 30, 160, 160, 3), dtype=float)
+i = 0 
 for message in consumer:
     stream = message.value
     #chuyển thành hình ảnh
@@ -39,32 +41,37 @@ for message in consumer:
     image = cv2.imdecode(np.frombuffer(stream, dtype=np.uint8), cv2.IMREAD_COLOR)
     print(image.shape)
 
-    cv2.putText(image, "PROCESSED", (100,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255),2)
+    # cv2.putText(image, "PROCESSED", (100,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255),2)
     # print("Send to kafka")
     ret, frame = cv2.imencode('.jpg', image)
-    print(frame)
+    # print(frame)
     if ret:   
         frm = resize(frame,(160,160,3))
         frm = np.expand_dims(frm,axis=0)
         if(np.max(frm)>1):
             frm = frm/255.0
-    print(frm)
+            print(frm)
+    while True:
+        if i < 30:
+            frames[i][:] = frm
+            nodatav[0][:][:] = frames
+            kq, percent = pred_fight(model, nodatav, acuracy=0.6)
+            i+=1
+        elif i == 29:
+            frames = np.delete(frames,0,axis=0)
+            frames[i][:] = frm         
+            nodatav[0][:][:] = frames
+            kq, percent = pred_fight(model, frames, acuracy=0.6)
+            print(kq)
+            print(frames.shape)
+            i+=1
+        print(kq)
+        print(frames)
+        # if kq: 
+        #         # producer.send(out_video_topic_name, frames.tobytes())
+        #         producer.send( out_topic_name , kq)#.encode("utf-8"))
+        # producer.send(out_video_topic_name, frame.tobytes)
+        # producer.flush()
+        
    
-frames = np.zeros((30, 160, 160, 3), dtype=np.float)
-i = 0 
-# 30 frames đầu tiên
 
-while True:
-    if i < 30:
-        kq, percent = pred_fight(model, frames, acuracy=0.6)
-    elif i == 29:
-        frames = np.delete(frames,0,axis=0)
-        frames[i][:] = frm         
-    kq, percent = pred_fight(model, frames, acuracy=0.6)
-    print(kq)
-    print(frames)
-    # if kq: 
-    #         # producer.send(out_video_topic_name, frames.tobytes())
-    #         producer.send( out_topic_name , kq)#.encode("utf-8"))
-    # producer.send(out_video_topic_name, frame.tobytes)
-    # producer.flush()
